451f0bb60343abbfd38a41707dd4da8045a176af
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-29 23:14:50 +0530

    classes in JS

--- OOP in JavaScript/app.js
+++ OOP in JavaScript/app.js
@@ -1,17 +1,35 @@
-function Person(name , age) {
-    this.name = name
-    this.age  = age
-    console.log(this);
+// Factory Function
+// function Person(name , age) {
+//     this.name = name
+//     this.age  = age
+//     console.log(this);
     
-}
+// }
 
-Person.prototype.talk= function () {
-    console.log(`Hi , I am ${this.name}`);
-}
+// Person.prototype.talk= function () {
+//     console.log(`Hi , I am ${this.name}`);
+// }
 
+// let p1 = new Person("lucky" , 20)
+// let p2  = new Person("darshan" , 19)
+
+// let c = p1.talk === p2.talk
+
+// console.log(c);
+
+// classes 
+
+class Person {
+    constructor(name , age) {
+        this.name
+        this.age
+    }
+    talk() {
+        console.log(`Hi I am ${name}`);
+        
+    }
+}
 let p1 = new Person("lucky" , 20)
 let p2  = new Person("darshan" , 19)
 
-let c = p1.talk === p2.talk
-
-console.log(c);
+console.log(p1.talk ===p2.talk);
