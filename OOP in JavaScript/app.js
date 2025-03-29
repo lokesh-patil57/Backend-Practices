d3814d4a50f467b46b6aba2b3e4c00e2abafc40b
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-29 23:39:09 +0530

    Inheritance concept in JS

--- OOP in JavaScript/app.js
+++ OOP in JavaScript/app.js
@@ -21,15 +21,33 @@
 
 class Person {
     constructor(name , age) {
-        this.name
-        this.age
+        this.name = name
+        this.age = age
     }
     talk() {
         console.log(`Hi I am ${name}`);
         
     }
 }
-let p1 = new Person("lucky" , 20)
-let p2  = new Person("darshan" , 19)
 
+class student extends Person {
+    constructor(name , age , marks) {
+        super(name, age)
+        this.marks = marks
+    }
+}
+
+class Teacher extends Person {
+    constructor(name , age , subject) {
+        super(name, age)
+        this.subject = subject
+    }
+}
+let p1 = new student("lucky" , 20, 95)
+let p2  = new Teacher("darshan" , 19, "Math")
+
+
+
+console.log(p1);
+console.log(p2);
 console.log(p1.talk ===p2.talk);
