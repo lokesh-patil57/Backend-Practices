7112ed24aeb654ac09b24b4b71fc1155a2daa789
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-29 14:56:29 +0530

    redable data in backend

--- get&Post/Backend/index.js
+++ get&Post/Backend/index.js
@@ -2,15 +2,23 @@ const express = require("express")
 const app = express()
 const port = 8080
 
+app.use(express.urlencoded({ extended:true}))
+app.use(express.json())
+
 app.get("/register" , (req , res ) => {
-    res.send("standard GET Response")
+    let{user , password} = req.query
+    res.send(`standard GET Response , Welcome ${user}`)
 })
 
 app.post("/register" ,(req , res) => {
-    res.send("standard POST Response")
+    let{user , password} = req.body
+    console.log(req.body);
+    
+    res.send(`standard POST Response, Welcome ${user}`)
 })
 
 app.listen( port , () =>{
     console.log(`listening on port :${port}`);
+    console.log(`Server running on http://localhost:${port}`);
     
 })
