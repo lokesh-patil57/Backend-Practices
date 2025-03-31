8c83f332d44ed029dd79b3dbfa7c6a1c2ce5b1ad
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-31 11:31:03 +0530

    new ejs file

--- RESTful API/index.js
+++ RESTful API/index.js
@@ -1,45 +1,54 @@
-const express = require("express")
-const app = express()
-const port = 8080
-const path = require("path")
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs")
-app.set("views" , path.join(__dirname ,"views"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.static(path.join(__dirname , "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
-    {
-        username : "luckypatil",
-        content : "Hi a MERN stack Developer!"
-    },
-    {
-        username : "ganeshpatil",
-        content : "Hi a FULL stack Developer!"
-    },
-    {
-        username : "sandy",
-        content : "Hi a Frontend Developer!"
-    }
-]
+  {
+    id: "1a",
+    username: "luckypatil",
+    content: "Hi a MERN stack Developer!",
+  },
+  {
+    id: "2b",
+    username: "ganeshpatil",
+    content: "Hi a FULL stack Developer!",
+  },
+  {
+    id: "3c",
+    username: "sandy",
+    content: "Hi a Frontend Developer!",
+  },
+];
 
-app.get("/posts" , (req , res)=>{
-    res.render("index.ejs", {posts})
-})
-app.get("/posts/new", (req , res )=>{
-    res.render("new.ejs")
-})
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
 
-app.post("/posts", (req, res)=>{
-        let { username , content} = req.body
-        posts.push({username , content})
-        res.redirect("/posts")
-})   
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  console.log(post);
+  res.render("show.ejs", { post });
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  posts.push({ username, content });
+  res.redirect("/posts");
+});
 
 app.listen(port, () => {
-    console.log(`listening on port :${port}`);
-    console.log(`Server running on http://localhost:${port}`);
-    
-})
+  console.log(`listening on port :${port}`);
+  console.log(`Server running on http://localhost:${port}`);
+});
