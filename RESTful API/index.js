1e26a6db1a5db1ec8f676543a0e7ed1a65cdeec0
lokesh-patil57 <luckypatil577R@gmail.com>
2025-03-31 12:58:46 +0530

    method-override package install

--- RESTful API/index.js
+++ RESTful API/index.js
@@ -2,8 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
-const { v4: uuidv4 } = require('uuid');
-
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,12 +18,12 @@ let posts = [
     content: "Hi a MERN stack Developer!",
   },
   {
-    id:uuidv4(),
+    id: uuidv4(),
     username: "ganeshpatil",
     content: "Hi a FULL stack Developer!",
   },
   {
-    id:uuidv4(),
+    id: uuidv4(),
     username: "sandy",
     content: "Hi a Frontend Developer!",
   },
@@ -44,10 +43,38 @@ app.get("/posts/:id", (req, res) => {
   res.render("show.ejs", { post });
 });
 
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newCont = req.body.content;
+
+  // Check if content is present in the request body
+  if (!newCont) {
+    return res.status(400).send({ error: "Content is required!" });
+  }
+
+  // Find the post by ID
+  let post = posts.find((p) => p.id === id);
+
+  if (!post) {
+    return res.status(404).send({ error: "Post not found!" });
+  }
+
+  // Update content and log the updated post
+  post.content = newCont;
+  console.log(post);
+  res.send("Post updated successfully!");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs" , {post});
+});
+
 app.post("/posts", (req, res) => {
   let { username, content } = req.body;
-  let id = uuidv4()
-  posts.push({id, username, content });
+  let id = uuidv4();
+  posts.push({ id, username, content });
   res.redirect("/posts");
 });
 
