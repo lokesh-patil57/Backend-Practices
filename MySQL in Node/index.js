7bc078f0966ad2c12f047004ef1b5a927c1ed8e4
lokesh-patil57 <luckypatil577R@gmail.com>
2025-04-23 19:37:21 +0530

    changes to user and querry

--- MySQL in Node/index.js
+++ MySQL in Node/index.js
@@ -8,16 +8,27 @@ const connection = mysql.createConnection({
     password : 'Lok22rk+'
 })
 
+let q = "INSERT INTO user ( id , username, email ,password ) values ?";
+let users = [
+  [ 1 , "lokesh" , "loki@gmail.com" , "Lok22as++"]
+  [ 2 , "bhavesh" , "burda@gmail.com" , "bhavesh729"]
+]; 
+
 try {
     connection.query("SHOW TABLES", (err, result ) =>{
             if (err) throw err
             console.log(result);
+            console.log(result.length);
+            console.log(result[0]);
+            console.log(result[1]);
             
     })
 } catch (err) {
     console.log(err);
     
 }
+
+connection.end();
 
 
 let getRandomUser = () => {
---
+++ MySQL in Node/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE user(
+    id int primary key,
+    username varchar(50) not null ,
+    email varchar(50) not null,
+    password varchar(50) not null
+);
