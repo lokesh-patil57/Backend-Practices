caefa58510a1f36b65ab1df16349a43a95ed11dd
lokesh-patil57 <luckypatil577R@gmail.com>
2025-04-24 23:53:41 +0530

    Ejs file and setup path in index.js file

--- MySQL in Node/index.js
+++ MySQL in Node/index.js
@@ -2,6 +2,10 @@ const { faker } = require("@faker-js/faker");
 const mysql = require("mysql2");
 const express = require("express")
 const app = express()
+const path = require("path")
+
+app.set("view engine" , "ejs")
+app.use("views", path.join(_dirname, "/views") )
 
 const connection = mysql.createConnection({
   host: "localhost",
