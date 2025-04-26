cd87b7caeec96844b9d445ce550be0748ed7798f
lokesh-patil57 <luckypatil577R@gmail.com>
2025-04-26 23:11:15 +0530

    home.ejs created

--- MySQL in Node/index.js
+++ MySQL in Node/index.js
@@ -30,8 +30,8 @@ app.get( "/" , (req , res)=>{
       try {
         connection.query(q,(err, result) => {
             if (err) throw err
-              console.log(result[0]["count(*)"]);
-            res.render("home.ejs")
+            let count = result[0]["count(*)"]
+            res.render("home.ejs", { count })
       }); 
     }catch (err) {
       console.log(err);
