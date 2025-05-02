89cc05ba25912cbdf00e6262ec3ad7bc5c7e9e16
lokesh-patil57 <luckypatil577R@gmail.com>
2025-05-02 23:30:32 +0530

    basic set up

---
+++ Mongo/index.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+
+app.listen(8080 ,()=>{
+    console.log("app is listening on port 8080");
+    console.log("http//:localhost:8080");
+    
+})
