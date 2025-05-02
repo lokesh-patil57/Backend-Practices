0de4be321e20da8bad44925754254141926ba13a
lokesh-patil57 <luckypatil577R@gmail.com>
2025-05-02 23:55:45 +0530

    properties of collections

--- Mongo/models/chat.js
+++ Mongo/models/chat.js
@@ -1,16 +1,20 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const chatSchema = new mongoose.Schema({
-    from : {
-        type :String,
-    },
-    to :{
-        type : String
-    },
-    msg :{
-        type : String
-    },
-    created_at :{
-        type : String
-    },
-})
+  from: {
+    type: String,
+    required: true,
+  },
+  to: {
+    type: String,
+    required: true,
+  },
+  msg: {
+    type: String,
+    maxLength: 50,
+  },
+  created_at: {
+    type: Date,
+    required: true,
+  },
+});
