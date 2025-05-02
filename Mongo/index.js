833758513c8b5d5a7a837d585b1396370cebe5c2
lokesh-patil57 <luckypatil577R@gmail.com>
2025-05-02 23:43:44 +0530

    path set views

--- Mongo/index.js
+++ Mongo/index.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
+const path = require("path")
 
-main().catch(err => console.log(err));
+app.set("views", path.join(__dirname, "views") )
+app.set("view engine", "ejs")
+
+main()
+.then(()=>{
+    console.log(`connection successful`);
+    
+})
+.catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
