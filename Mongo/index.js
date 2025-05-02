a11b7114007161ac168aac60f1a195fa9878346b
lokesh-patil57 <luckypatil577R@gmail.com>
2025-05-02 23:39:34 +0530

    whatsapp database mongo

--- Mongo/index.js
+++ Mongo/index.js
@@ -1,5 +1,12 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
 
 app.get("/" ,(req , res)=>{
     res.send("working")
