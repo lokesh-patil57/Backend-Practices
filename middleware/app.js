22f04f54b2b869f2bbe8f535029134927dfd5e18
lokesh-patil57 <luckypatil577R@gmail.com>
2025-06-09 15:52:25 +0530

    middleware path not defined

--- middleware/app.js
+++ middleware/app.js
@@ -1,11 +1,20 @@
 const express = require("express")
 const app = express()
 
+app.use((req,res) =>{
+    res.send("I am middleware")
+})
+
+app.post("/random" , (req , res )=>{
+    res.send("I am Random")
+})
+
+app.get("/" , (req , res ) =>{
+    res.send("Hi I am root")
+})
+
 app.listen("8080", () => {
   console.log(`app is listening on port 8080`);
   console.log(`http://localhost:8080`);
 });
 
-app.get("/" , (req , res ) =>{
-    res.send("Hi I am root")
-})
