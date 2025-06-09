620cc4767c2e3580f10f950d1649f9e2c22a7ee9
lokesh-patil57 <luckypatil577R@gmail.com>
2025-06-09 15:59:52 +0530

    next variable use

--- middleware/app.js
+++ middleware/app.js
@@ -1,20 +1,27 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use((req,res) =>{
-    res.send("I am middleware")
-})
+app.use((req, res, next) => {
+  console.log(`I am 1st middleware`);
 
-app.post("/random" , (req , res )=>{
-    res.send("I am Random")
-})
+  next();
+});
 
-app.get("/" , (req , res ) =>{
-    res.send("Hi I am root")
-})
+app.use((req, res , next) => {
+  console.log(`I am 2nd middleware`);
+
+  next();
+});
+
+app.get("/random", (req, res) => {
+  res.send("I am Random");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi I am root");
+});
 
 app.listen("8080", () => {
   console.log(`app is listening on port 8080`);
   console.log(`http://localhost:8080`);
 });
-
