da5c490befc57eac4bbdfbdcee43f338946ba962
Lucky Patil <luckypatil577R@gmail.com>
2025-09-08 09:28:43 +0530

    Store and save the session

--- Cookie&Session/server.js
+++ Cookie&Session/server.js
@@ -2,22 +2,33 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 
-app.use(
-  session({
+const sessionOptions = {
     secret: "mysuperSecret",
-    resave:false,
-    saveUninitialized:true,
-  })
-);
+    resave: false,
+    saveUninitialized: true,
+  }
 
-app.get("/reqcount" , (req,res)=>{
-    if (req.session.count) {
-        req.session.count++;
-    } else{
-        req.session.count = 1;
-    }
-    res.send(`You sent a Request ${req.session.count} times`)
+app.use(session(sessionOptions))
+
+app.get("/register" , (req,res) =>{
+    let {name="anonymous"} = req.query
+    req.session.name = name
+    console.log(req.session.name)
+    res.redirect("/hello")
 })
+
+app.get("/hello", (req,res)=>{
+    res.send(`Hello ${req.session.name}`)
+})
+
+app.get("/reqcount", (req, res) => {
+  if (req.session.count) {
+    req.session.count++;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`You sent a Request ${req.session.count} times`);
+});
 
 // app.get("/test", (req,res)=>{
 //     res.send("test is working")
