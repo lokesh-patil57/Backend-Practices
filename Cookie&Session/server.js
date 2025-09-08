3ae8b5f43d3fb7a3726cae33a49525befa2b2d9e
Lucky Patil <luckypatil577R@gmail.com>
2025-09-08 09:17:20 +0530

    Session count

--- Cookie&Session/server.js
+++ Cookie&Session/server.js
@@ -1,3 +1,33 @@
-const express = require("express")
-const app = express()
-const session = require("express-session")
+const express = require("express");
+const app = express();
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "mysuperSecret",
+    resave:false,
+    saveUninitialized:true,
+  })
+);
+
+app.get("/reqcount" , (req,res)=>{
+    if (req.session.count) {
+        req.session.count++;
+    } else{
+        req.session.count = 1;
+    }
+    res.send(`You sent a Request ${req.session.count} times`)
+})
+
+// app.get("/test", (req,res)=>{
+//     res.send("test is working")
+// })
+
+app.get("/", (req, res) => {
+  res.send("I am Root");
+});
+
+app.listen(3000, () => {
+  console.log("Server is started on port 3000");
+  console.log("http://localhost:3000/");
+});
